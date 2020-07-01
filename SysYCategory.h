b63cae7f23433b737347b3be997efd28bb4d49c3
YikeZhou <u201714805@hust.edu.cn>
2020-07-01 13:20:48 +0800

    :construction: Add category enums.

---
+++ SysYCategory.h
@@ -0,0 +1,26 @@
+/*
+ * @Date: 2020-07-01 11:31:14
+ * @LastEditors: zyk
+ * @LastEditTime: 2020-07-01 13:17:56
+ * @FilePath: \compiler\SysYCategory.h
+ */
+
+#ifndef _SYSYCATEGORY_H
+#define _SYSYCATEGORY_H
+
+// 符号类型
+typedef enum SymbolCategory {
+  VarArray,   // 数组变量
+  ConstArray, // 数组常量
+  VarInt,     // 整型变量
+  ConstInt,   // 整型常量
+  Function,   // 函数
+  LibFunc,    // 库函数
+  Block,      // 内嵌语句块
+  None        // 缺省类型
+} SymbolCategory;
+
+// 返回值类型
+typedef enum ReturnCategory { Void, Integer } ReturnCategory;
+
+#endif // _SYSYCATEGORY_H
