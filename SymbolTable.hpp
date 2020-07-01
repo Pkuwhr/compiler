ad140125da2d5ca39e50a301252bb249925c9609
YikeZhou <u201714805@hust.edu.cn>
2020-07-01 15:07:01 +0800

    :construction: 初步完成符号表结构定义

---
+++ SymbolTable.hpp
@@ -0,0 +1,49 @@
+/*
+ * @Date: 2020-07-01 12:45:00
+ * @LastEditors: zyk
+ * @LastEditTime: 2020-07-01 15:06:35
+ * @FilePath: \compiler\SymbolTable.hpp
+ */
+
+#ifndef _SYMBOLTABLE_H
+#define _SYMBOLTABLE_H
+
+#include <cstdlib>
+#include <vector>
+
+#include "ArrayInfo.hpp"
+#include "SysYCategory.h"
+
+using namespace std;
+
+// 局部作用域符号表
+typedef struct LocalScopeEntry {
+  char *name;
+  SymbolCategory symbol_type;
+  vector<LocalScopeEntry> *embedded_scope; // 内嵌域符号表
+} LocalScopeEntry;
+
+typedef vector<LocalScopeEntry> LocalScope;
+
+// 函数形参作用域符号表
+typedef struct FormalScopeEntry {
+  char *name;
+  SymbolCategory formal_type; // 形参类型
+} FormalScopeEntry;
+
+typedef vector<FormalScopeEntry> FormalScope;
+
+typedef struct GlobalScopeEntry {
+  char *name;
+  SymbolCategory symbol_type; // 全局符号类型
+  ReturnCategory rev_type;    // 函数返回值类型
+  union {
+    int init_val;          // 整型常量初值
+    int formal_count;      // 函数形参个数
+    ArrayInfo *array_info; // 数组信息
+  };
+  FormalScope *formal_scope; // 函数形参域
+  LocalScope *local_scope;   // 函数体域
+};
+
+#endif // _SYMBOLTABLE_H
